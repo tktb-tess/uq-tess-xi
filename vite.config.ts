51a36a3e4c0aeb9228825847d5c7cb7a521cdfab
Born-Bohrer <chem1000leaves@gmail.com>
2025-03-28 01:20:31 +0900

    ???

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import wasm from 'vite-plugin-wasm';
+import topLvAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
 		sveltekit(),
-	]
+		wasm(),
+		topLvAwait(),
+	],
+	server: {
+		open: '/.',
+	},
+	optimizeDeps: {
+		exclude: [
+			
+		],
+	}
 });
