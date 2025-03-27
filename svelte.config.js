0a6dc08baaf25bb2f6af0cd78e2a84230c1ea80b
Born-Bohrer <chem1000leaves@gmail.com>
2025-03-28 01:03:56 +0900

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const config = {
+	preprocess: [
+		vitePreprocess(),
+	],
+	kit: { 
+		adapter: adapter() 
+	},
+	extensions: [
+		'.svelte',
+	]
+};
+
+export default config;
