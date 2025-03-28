297834ad9565d16a6a1c28c4b84013c35cfa6d23
Born-Bohrer <chem1000leaves@gmail.com>
2025-03-28 13:32:47 +0900

    一旦

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,20 @@
-<p>EMPTY</p>
+<main class="grid place-content-center place-items-center min-h-screen overflow-hidden">
+    <p class="text-6xl font-extralight">Hallo, Welt!</p>
+</main>
+
+<style>
+    p {
+        animation: vibe 8s infinite;
+        animation-timing-function: cubic-bezier(0, .95, 1, .05);
+    }
+
+    @keyframes vibe {
+        0% {
+            transform: translateX(-60vw);
+        }
+
+        100% {
+            transform: translateX(60vw);
+        }
+    }
+</style>
