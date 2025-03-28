7b0c0839b735079c1c170147acf9baed9726b66a
Born-Bohrer <chem1000leaves@gmail.com>
2025-03-28 18:45:34 +0900

    ittan

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,19 @@
-<main class="grid place-content-center place-items-center min-h-screen overflow-hidden">
-    <p class="text-6xl font-extralight">Hallo, Welt!</p>
+<main class="flex flex-col gap-y-1 justify-center items-center min-h-screen overflow-clip">
+    <p class="text-8xl font-serif italic">Hallo, Welt!</p>
 </main>
 
 <style>
-    p {
-        animation: vibe 8s infinite;
+    main p {
+        animation: vibe 10s infinite;
         animation-timing-function: cubic-bezier(0, .95, 1, .05);
     }
 
     @keyframes vibe {
-        0% {
-            transform: translateX(-60vw);
+        from {
+            transform: translateX(-70vw);
         }
-
-        100% {
-            transform: translateX(60vw);
+        to {
+            transform: translateX(70vw);
         }
     }
 </style>
