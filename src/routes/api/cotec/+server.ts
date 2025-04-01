0cfa4b40b27f86d734bed05b6fb14a90e1b00b1e
Born-Bohrer <chem1000leaves@gmail.com>
2025-04-01 14:39:47 +0900

    test

--- src/routes/api/cotec/+server.ts
+++ src/routes/api/cotec/+server.ts
@@ -1,22 +1,7 @@
 import { parseToJSON } from "$lib/modules/fetching";
 
-const allowed_origins = [
-    'https://conlang-gacha.vercel.app',
-    'http://localhost:5173',
-] as const;
-
-export const GET = async ({ url, request, setHeaders }) => {
+export const GET = async ({ url }) => {
     console.log('receive GET request at /cotec');
-
-    const origin = request.headers.get('Origin');
-
-    allowed_origins.forEach((allowed) => {
-        if (allowed === origin) {
-            setHeaders({
-                'Access-Control-Allow-Origin': origin,
-            });
-        }
-    });
 
     const [metadata, contents] = await parseToJSON();
 
