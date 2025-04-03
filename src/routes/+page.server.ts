92f31f69ffa287406a42219fc3c9fd6bd156ea2f
Born-Bohrer <chem1000leaves@gmail.com>
2025-04-04 02:04:57 +0900

    rayleigh

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,8 +3,7 @@ import vaesOtmJson from '$lib/assets/vl-ja.otm.json';
 export const prerender = false;
 
 export const load = async () => {
-    const date = 'test';
-    
+    const date = new Date().toDateString();
     const encoded = Buffer.from(date);
     const hash = await crypto.subtle.digest('SHA-256', encoded);
     const rand = new Uint32Array(hash, 0, 1)[0];
