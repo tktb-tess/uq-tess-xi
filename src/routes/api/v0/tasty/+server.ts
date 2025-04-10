76f40f2b72d58c189b00d633603adef99bdae445
Born-Bohrer <chem1000leaves@gmail.com>
2025-04-10 23:00:09 +0900

    sho-mona

--- src/routes/api/v0/tasty/+server.ts
+++ src/routes/api/v0/tasty/+server.ts
@@ -4,6 +4,6 @@ import { createClient } from "redis";
 
 export const GET = async () => {
     const redis = await createClient({ url: REDIS_URL }).connect();
-    console.log(redis.json.get('today-word'));
-    return json(null);
+    const a = await redis.json.get('today-word');
+    return json(a);
 }
