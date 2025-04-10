1d96ba1d2c8aeff7dc578b2bc54d40057c3958af
Born-Bohrer <chem1000leaves@gmail.com>
2025-04-10 10:16:39 +0900

    ddd

--- src/lib/modules/zpdic.ts
+++ src/lib/modules/zpdic.ts
@@ -1,6 +1,4 @@
 import otm_json from '../assets/vl-ja.otm.json';
-import { ZPDIC_API_KEY } from '$env/static/private';
-import { getRndInt } from './util';
 
 export type ZpDICWord = typeof otm_json.words[0];
 export type ZpDICExample = typeof otm_json.examples[0];
@@ -60,40 +58,6 @@ export type ZpDICAPIResponseWord = {
 export type ZpDICAPIWordsResponse = {
     words: ZpDICAPIResponseWord[];
     total: number;
-};
-
-const api_url = `http://zpdic.ziphil.com/api/v0/dictionary/633/words`;
-
-const fetchZpDIC = async (url: string) => {
-    const headers = {
-        'X-Api-Key': ZPDIC_API_KEY,
-    };
-
-    const options = {
-        headers,
-    };
-
-    const response = await fetch(url, options);
-
-    if (!response.ok) throw Error(`failed to fetch\nstatus: ${response.status}`);
-
-    return response.json() as Promise<ZpDICAPIWordsResponse>;
-};
-
-const getWordCount = async () => {
-    const url = `${api_url}?text=`;
-
-    const json = await fetchZpDIC(url);
-
-    return json.total;
-};
-
-export const getWord = async () => {
-    const total = await getWordCount();
-    const rand = getRndInt(0, total);
-    const url = `${api_url}?text=&skip=${rand}&limit=1`;
-    const resp = await fetchZpDIC(url);
-    return resp.words[0];
 };
 
 
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,21 +5,21 @@ import vaes_otm_json from '$lib/assets/vl-ja.otm.json';
 export const prerender = false;
 
 export const load = async () => {
-    const date = TZDate.tz('Asia/Tokyo');
-    const encoded = Buffer.from(date.toDateString());
-    const rand = new Uint32Array(await crypto.subtle.digest('SHA-256', encoded), 0, 1)[0];
-    const length = vaes_otm_json.words.length;
-    const today_word = vaes_otm_json.words[rand % length];
+	const date = TZDate.tz('Asia/Tokyo');
+	const encoded = Buffer.from(date.toDateString());
+	const rand = new Uint32Array(await crypto.subtle.digest('SHA-256', encoded), 0, 1)[0];
+	const length = vaes_otm_json.words.length;
+	const today_word = vaes_otm_json.words[rand % length];
 
-    const query = `?text=${encodeURI(today_word.entry.form)}&mode=name&type=exact`;
-    const dic_url = `https://zpdic.ziphil.com/dictionary/vaessenzlaendiskj${query}`;
+	const query = `?kind=exact&number=${today_word.entry.id}`;
+	const dic_url = `https://zpdic.ziphil.com/dictionary/633${query}`;
 
-    const pron = today_word.contents.find((cont) => cont.title === 'Pronunciation')?.text;
+	const pron = today_word.contents.find((cont) => cont.title === 'Pronunciation')?.text;
 
-    const size = (() => {
-        const len = today_word.entry.form.length;
-        return len < 10 ? 'text-5xl' : 'text-4xl';
-    })();
+	const size = (() => {
+		const len = today_word.entry.form.length;
+		return len < 10 ? 'text-5xl' : 'text-4xl';
+	})();
 
-    return { today_word, dic_url, pron, size } as const;
+	return { today_word, dic_url, pron, size } as const;
 };
