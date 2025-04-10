301e45d80391e2646447f1057880633f68f2a707
Born-Bohrer <chem1000leaves@gmail.com>
2025-04-10 22:29:17 +0900

    test1

---
+++ src/routes/api/v0/tasty/+server.ts
@@ -0,0 +1,10 @@
+import { REDIS_URL } from "$env/static/private";
+import { json } from "@sveltejs/kit";
+import { createClient } from "redis";
+
+export const GET = async () => {
+    const redis = await createClient({ url: REDIS_URL }).connect();
+
+    const a = redis.json.get('today-word');
+    return json(a);
+}
