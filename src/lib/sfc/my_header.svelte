ae7c90575ae98e3950cb1ab11ebaf7606176e012
Born-Bohrer <chem1000leaves@gmail.com>
2025-04-17 13:22:25 +0900

    ssejn

--- src/lib/sfc/my_header.svelte
+++ src/lib/sfc/my_header.svelte
@@ -2,10 +2,10 @@
 	
 </script>
 
-<header class="bg-mnlila text-white [&_a]:text-white [&_a]:hover:text-white/70 [&_a]:transition-colors [&_a]:no-underline [&_a]:h-[64px] [&_a]:flex [&_a]:items-center">
-    <div class="flex *:flex-[0_0_auto] mx-auto max-w-[75%] h-full justify-between gap-x-5">
+<header class="bg-mnlila text-white [&_a]:text-white [&_a]:hover:text-white/70 [&_a]:transition-colors [&_a]:no-underline [&_a]:h-[64px] [&_a]:flex [&_a]:items-center [&_a]:truncate">
+    <div class="flex *:flex-[0_1_auto] *:min-w-0 mx-auto max-w-[75%] h-full justify-between gap-x-5">
         <h1 class="font-serif text-3xl"><a class="" href="/.">悠久肆方体</a></h1>
-        <nav class="flex gap-x-5">
+        <nav class="flex *:min-w-0 *:flex-[0_1_auto] gap-x-5">
             <a class="" href="/vaes">Vässenzländisķ</a>
             <a class="" href="/data">データ</a>
             <a class="" href="/others">その他</a>
