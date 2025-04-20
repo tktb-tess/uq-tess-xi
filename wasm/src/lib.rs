5d5f6dcef74cb283b48775b9660b5f6b6c0b306b
Born-Bohrer <chem1000leaves@gmail.com>
2025-04-20 18:07:37 +0900

    ittan moment

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,6 +1,24 @@
-use wasm_bindgen::prelude::*;
+use num_bigint::{BigInt, BigUint};
+//use js_sys::BigInt as jsBigInt;
 
-#[wasm_bindgen(main)]
-fn main() {
-    println!("Hello, wasm!");
+pub fn mod_pow(b: &BigUint, p: &BigUint, m: &BigUint) -> BigUint {
+    let one = BigUint::from(1u8);
+
+    if (b % m == one) || (b % m == BigUint::ZERO) {
+        return b.clone();
+    } else if *b == m - &one {
+        return if p % BigUint::from(2u8) == one { m - one } else { one };
+    };
+
+    let mut res = one.clone();
+
+    while *p > BigUint::ZERO {
+        if p & &one == one {
+            res = res * b % m;
+        };
+        
+    }
+
+    res
 }
+
