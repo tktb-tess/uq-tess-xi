f56d2b4a1b312eef98c10dd960276f76d5aafe29
Born-Bohrer <chem1000leaves@gmail.com>
2025-04-28 00:59:22 +0900

    ittan moment

--- src/routes/(layout1)/vaes/letter-et-pron/+page.svelte
+++ src/routes/(layout1)/vaes/letter-et-pron/+page.svelte
@@ -1,10 +1,570 @@
 <script lang="ts">
-
 </script>
 
 <svelte:head>
-    <title>葦語 文字と発音</title>
+	<title>葦語 文字と発音</title>
 </svelte:head>
 
-<h2 class="my-8 text-center">文字と発音</h2>
-<p class="text-red-600 text-xl text-center">工事中……</p>
+<h2 class="text-center my-8">文字と発音</h2>
+
+<h2 class="border-b-3 border-double ps-1">単独字の発音</h2>
+<p>
+	表記にはラテン文字を使用する。a&ndash;zの26文字に加え、いくつかのダイアクリティカルマークを用いる。
+</p>
+<p>備考に特に何も書かれていなければ、ローマ字通りの発音でOK。</p>
+<p>「発音例」の音声記号は、「音韻論」の項で詳しく説明している。</p>
+
+<div class="table-container">
+	<table class="grid-cols-[repeat(4,auto)] [&_td:is(:first-child,:nth-child(2))]:text-center [&_td:nth-child(3)]:font-ipa">
+		<thead>
+			<tr>
+				<th scope="col">大文字</th>
+				<th scope="col">小文字</th>
+				<th scope="col">発音例</th>
+				<th scope="col">備考</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>A</td>
+				<td>a</td>
+				<td>/ɐ/, /ɑː/</td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>Ä</td>
+				<td>ä</td>
+				<td>/ɛ(ː)/, /ɤ/</td>
+				<td>少し広めの「エ」</td>
+			</tr>
+			<tr>
+				<td>Ą</td>
+				<td>ą</td>
+				<td>/ɔ̃/, /ɔ̃ː/</td>
+				<td>鼻に息を通しながら「オ」。少し広め</td>
+			</tr>
+			<tr>
+				<td>Ą̈</td>
+				<td>ą̈</td>
+				<td>/ɛ̃(ː)/, /ɤ̃/</td>
+				<td>鼻に息を通しながら「エ」</td>
+			</tr>
+			<tr>
+				<td>B</td>
+				<td>b</td>
+				<td>/b/</td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>C</td>
+				<td>c</td>
+				<td>/k/, /s/, /t͡s/ etc.</td>
+				<td>借用語を除いては、下の重音字でしか基本用いられない</td>
+			</tr>
+			<tr>
+				<td>D</td>
+				<td>d</td>
+				<td>/d/</td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>Ð</td>
+				<td>ð</td>
+				<td>/ð/</td>
+				<td>英語 <i>this</i> の <i>th</i> に近い</td>
+			</tr>
+			<tr>
+				<td>E</td>
+				<td>e</td>
+				<td>/e(ː)/, /ɤ/</td>
+				<td> <i>ä</i> より口を狭めた「エ」</td>
+			</tr>
+			<tr>
+				<td>Ę</td>
+				<td>ę</td>
+				<td>/ẽ(ː)/, /ɤ̃/</td>
+				<td>鼻に息を通しながら <i>e</i>。<i>ą̈</i> より狭い</td>
+			</tr>
+			<tr>
+				<td>F</td>
+				<td>f</td>
+				<td>/f/</td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>G</td>
+				<td>g</td>
+				<td>/ɡ/, /ʑ/</td>
+				<td><span class="font-ipa">/ʑ/</span> は外来語に現れる</td>
+			</tr>
+			<tr>
+				<td>Ģ</td>
+				<td>ģ</td>
+				<td>/j/, /ɪ/, /iː/</td>
+				<td> <i>j</i> と同じ</td>
+			</tr>
+			<tr>
+				<td>H</td>
+				<td>h</td>
+				<td>/ɦ/</td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>I</td>
+				<td>i</td>
+				<td>/ɪ/, /iː/</td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>Į</td>
+				<td>į</td>
+				<td>/ẽ(ː)/</td>
+				<td> <i>ę</i> と同じ</td>
+			</tr>
+			<tr>
+				<td>J</td>
+				<td>j</td>
+				<td>/j/</td>
+				<td>ジャ行ではなくヤ行</td>
+			</tr>
+			<tr>
+				<td>K</td>
+				<td>k</td>
+				<td>/k/</td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>Ķ</td>
+				<td>ķ</td>
+				<td>/ɕ/</td>
+				<td>日本語のシャ行と大体同じ</td>
+			</tr>
+			<tr>
+				<td>L</td>
+				<td>l</td>
+				<td>/l/</td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>M</td>
+				<td>m</td>
+				<td>/m/, /˜/</td>
+				<td>直前の母音を鼻母音化することがある</td>
+			</tr>
+			<tr>
+				<td>N</td>
+				<td>n</td>
+				<td>/n/, /˜/, (/ŋ/)</td>
+				<td>直前の母音を鼻母音化することがある</td>
+			</tr>
+			<tr>
+				<td>O</td>
+				<td>o</td>
+				<td>/ɔ/, /oː/</td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>Ö</td>
+				<td>ö</td>
+				<td>/œ/, /øː/</td>
+				<td>「オ」の口の形のまま「エ」と言う。ドイツ語の <i>ö</i> と大体同じ</td>
+			</tr>
+			<tr>
+				<td>Ǫ</td>
+				<td>ǫ</td>
+				<td>/ɔ̃/, /õː/</td>
+				<td>長母音の時、<i>ą</i> より若干狭い。短母音の時同じ</td>
+			</tr>
+			<tr>
+				<td>Ǫ̈</td>
+				<td>ǫ̈</td>
+				<td>/œ̃/, /ø̃ː/</td>
+				<td><i>ö</i> を鼻に息を通しながら発音する</td>
+			</tr>
+			<tr>
+				<td>P</td>
+				<td>p</td>
+				<td>/p/</td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>Q</td>
+				<td>q</td>
+				<td>&#x2013;</td>
+				<td
+					>基本 <i>qu</i> のセットで使われ、発音は <span class="font-ipa">/kʋ/</span>。<i>kv</i> と同じ
+				</td>
+			</tr>
+			<tr>
+				<td>R</td>
+				<td>r</td>
+				<td>/ɽ/, /ɻ/ etc.</td>
+				<td>日本語のラ行子音に近いが、少し奥の方。詳しくは「音韻論」を参照のこと</td>
+			</tr>
+			<tr>
+				<td>S</td>
+				<td>s</td>
+				<td>/s/</td>
+				<td>英語のようにザ行を表すことはほぼない</td>
+			</tr>
+			<tr>
+				<td>T</td>
+				<td>t</td>
+				<td>/t/</td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>U</td>
+				<td>u</td>
+				<td>/ʊ/, /uː/</td>
+				<td>日本語の「ウ」よりも奥の方で唇をすぼめて発音する</td>
+			</tr>
+			<tr>
+				<td>Ü</td>
+				<td>ü</td>
+				<td>/ʏ/, /yː/</td>
+				<td>「ウ」の口の形のまま「イ」と言う。ドイツ語の <i>ü</i> と大体同じ</td>
+			</tr>
+			<tr>
+				<td>Ų</td>
+				<td>ų</td>
+				<td>/ɔ̃/, /õː/</td>
+				<td><i>ǫ</i> と同じ</td>
+			</tr>
+			<tr>
+				<td>Ų̈</td>
+				<td>ų̈</td>
+				<td>/œ̃/, /ø̃ː/</td>
+				<td><i>ǫ̈</i> と同じ</td>
+			</tr>
+			<tr>
+				<td>V</td>
+				<td>v</td>
+				<td>/ʋ/, /f/</td>
+				<td>英語の <i>v</i> よりも摩擦が弱い。ワ行にも聞こえる</td>
+			</tr>
+			<tr>
+				<td>W</td>
+				<td>w</td>
+				<td>/ʋ/, /ʊ̯/</td>
+				<td>借用語を除いては、下の重音字でしか基本用いられない</td>
+			</tr>
+			<tr>
+				<td>X</td>
+				<td>x</td>
+				<td>/ks/</td>
+				<td><i>ks</i> と同じ</td>
+			</tr>
+			<tr>
+				<td>Y</td>
+				<td>y</td>
+				<td>/ʏ/, /yː/</td>
+				<td><i>ü</i> と同じ</td>
+			</tr>
+			<tr>
+				<td>Ỹ</td>
+				<td>ỹ</td>
+				<td>/œ̃/, /ø̃ː/</td>
+				<td><i>ų̈</i> と同じ</td>
+			</tr>
+			<tr>
+				<td>Z</td>
+				<td>z</td>
+				<td>/z/, /s/</td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>Ź</td>
+				<td>ź</td>
+				<td>/ʑ/</td>
+				<td>大体日本語のジャ行と同じ</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+
+<h2 class="border-b-3 border-double ps-1">重音字の発音</h2>
+<p>
+	いくつかの子音や母音は二文字以上の文字の組み合わせで表される。ここでは、そのようなものを<span class="font-bold"
+		>重音字</span
+	>と呼んでいる。
+</p>
+<div class="table-container">
+	<table class="grid-cols-[repeat(3,auto)] [&_td:first-child]:text-center [&_td:nth-child(2)]:font-ipa">
+		<thead>
+			<tr>
+				<th scope="col">文字</th>
+				<th scope="col">発音例</th>
+				<th scope="col">備考</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>pb</td>
+				<td>/ʔ͡p/</td>
+				<td>前声門化破裂音</td>
+			</tr>
+			<tr>
+				<td>td</td>
+				<td>/ʔ͡t/</td>
+				<td>〃</td>
+			</tr>
+			<tr>
+				<td>kg</td>
+				<td>/ʔ͡k/</td>
+				<td>〃</td>
+			</tr>
+			<tr>
+				<td>同じ子音字 × 2</td>
+				<td>&#x2013;</td>
+				<td>直前の母音が短母音であることを表す。ただし <i>mm, nn</i> を除く</td>
+			</tr>
+			<tr>
+				<td>sķ</td>
+				<td>/ɬ/</td>
+				<td><i>l</i> の口のまま、ヒャ行を言う感じ</td>
+			</tr>
+			<tr>
+				<td>gh</td>
+				<td>/x/</td>
+				<td>喉の奥から勢いよくハ行を言う感じ。ドイツ語の <i>ch</i> などに近い</td>
+			</tr>
+			<tr>
+				<td>語末のmb</td>
+				<td>/m/</td>
+				<td>bは読まない</td>
+			</tr>
+			<tr>
+				<td>語末のnd</td>
+				<td>/n/</td>
+				<td>dは読まない</td>
+			</tr>
+			<tr>
+				<td>ng</td>
+				<td>/ŋ/</td>
+				<td
+					>ング。英語の <i>ng</i> とほぼ変わらない。<span class="font-ipa">/ŋɡ/</span> ではないので注意</td
+				>
+			</tr>
+			<tr>
+				<td>nj</td>
+				<td>/ɲ/</td>
+				<td>ニュ。フランス語の <i>gn</i> や日本語のニャ行とほぼ同じ</td>
+			</tr>
+			<tr>
+				<td>hv</td>
+				<td>/f/, /xʋ/</td>
+				<td>疑問詞によく出てくる。前者の方がより一般的</td>
+			</tr>
+			<tr>
+				<td>aa</td>
+				<td>/ɑː/</td>
+				<td>長い <i>a</i> と同じ</td>
+			</tr>
+			<tr>
+				<td>ąa</td>
+				<td>/ɔ̃ː/</td>
+				<td>鼻に息を通しながら <i>aa</i> と発音</td>
+			</tr>
+			<tr>
+				<td>ea, äa</td>
+				<td>/ɛː/</td>
+				<td>長い <i>ä</i> と同じ</td>
+			</tr>
+			<tr>
+				<td>ęa, ą̈a</td>
+				<td>/ɛ̃ː/</td>
+				<td>鼻に息を通しながら <i>ea</i> と発音</td>
+			</tr>
+			<tr>
+				<td>ee</td>
+				<td>/eː/</td>
+				<td>長い <i>e</i> と同じ</td>
+			</tr>
+			<tr>
+				<td>ęe</td>
+				<td>/ẽː/</td>
+				<td>鼻に息を通しながら <i>ee</i> と発音</td>
+			</tr>
+			<tr>
+				<td>ie</td>
+				<td>/iː/</td>
+				<td>長い <i>i</i> と同じ</td>
+			</tr>
+			<tr>
+				<td>įe</td>
+				<td>/ẽː/</td>
+				<td><i>ęe</i> と同じ</td>
+			</tr>
+			<tr>
+				<td>au</td>
+				<td>/oː/</td>
+				<td>長い <i>o</i> と同じ</td>
+			</tr>
+			<tr>
+				<td>ąu</td>
+				<td>/õː/</td>
+				<td>鼻に息を通しながら <i>au</i> と発音</td>
+			</tr>
+			<tr>
+				<td>eu, äu</td>
+				<td>/øː/</td>
+				<td>長い <i>ö</i> と同じ</td>
+			</tr>
+			<tr>
+				<td>ęu, ą̈u</td>
+				<td>/ø̃ː/</td>
+				<td>鼻に息を通しながら <i>eu</i> と発音</td>
+			</tr>
+			<tr>
+				<td>ou</td>
+				<td>/uː/</td>
+				<td>長い <i>u</i> と同じ</td>
+			</tr>
+			<tr>
+				<td>ǫu</td>
+				<td>/õː/</td>
+				<td><i>ąu</i> と同じ</td>
+			</tr>
+			<tr>
+				<td>ey, öu</td>
+				<td>/yː/</td>
+				<td>長い <i>y</i> と同じ</td>
+			</tr>
+			<tr>
+				<td>ey, ǫ̈u</td>
+				<td>/ø̃ː/</td>
+				<td><i>ęu</i> と同じ</td>
+			</tr>
+			<tr>
+				<td>ai</td>
+				<td>/aɪ/</td>
+				<td>ひと息に「アイ」</td>
+			</tr>
+			<tr>
+				<td>ąi</td>
+				<td>/ɐɲ/</td>
+				<td><i>anj</i> と同じ</td>
+			</tr>
+			<tr>
+				<td>äi, ei, äģ, eģ</td>
+				<td>/ɛɪ/</td>
+				<td>ひと息に「エイ」</td>
+			</tr>
+			<tr>
+				<td>ą̈i, ęi, ą̈ģ, ęģ</td>
+				<td>/ɛɲ/</td>
+				<td><i>änj</i> と同じ</td>
+			</tr>
+			<tr>
+				<td>aw</td>
+				<td>/ɑʊ/</td>
+				<td>ひと息に「アウ」。<i>au</i> とは違うので注意</td>
+			</tr>
+			<tr>
+				<td>ąw</td>
+				<td>/ɐŋ/</td>
+				<td><i>ang</i> と同じ</td>
+			</tr>
+			<tr>
+				<td>ew, äw</td>
+				<td>/joː/</td>
+				<td>「エウ」ではなく「ヨー」。<i>eu</i> とは違うので注意</td>
+			</tr>
+			<tr>
+				<td>ęw, ą̈w</td>
+				<td>/jõː/</td>
+				<td>鼻に息を通しながら <i>ew</i> と発音</td>
+			</tr>
+			<tr>
+				<td>uy</td>
+				<td>/œʏ/</td>
+				<td>口をすぼめつつ「エイ」「ウイ」と言う感じ。オランダ語の <i>ui</i> とほぼ同じ</td>
+			</tr>
+			<tr>
+				<td>ųy</td>
+				<td>/œɲ/</td>
+				<td><i>önj</i> と同じ</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+
+<h2 class="border-b-3 border-double ps-1">鼻母音の表記について</h2>
+<p>
+	基本は、「母音字 + <i>m</i>」or「母音字 +
+	<i>n</i
+	>」という綴りのとき、対応する鼻母音で読む。基本はそのままだが、下に示すように母音が変わるものもある。
+</p>
+<div class="table-container">
+	<table class="grid-cols-[repeat(2,auto)] [&_td:nth-child(2)]:font-ipa">
+		<thead>
+			<tr>
+				<th>表記</th>
+				<th>発音</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>an, am</td>
+				<td>/ɔ̃(ː)/</td>
+			</tr>
+			<tr>
+				<td>in, im</td>
+				<td>/ẽ(ː)/, /ɤ̃/</td>
+			</tr>
+			<tr>
+				<td>yn, ün, ym, üm</td>
+				<td>/œ̃/, /ø̃ː/</td>
+			</tr>
+			<tr>
+				<td>un, um</td>
+				<td>/ɔ̃/, /õː/</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+
+<p>ただし、</p>
+<ul>
+	<li><i>nd, nt, nk, ng, mb, mp</i> という綴りの場合</li>
+	<li><i>m, n</i> の左右両隣が母音字である場合</li>
+	<li><i>mm, nn</i> と二重に綴ってある場合</li>
+</ul>
+<p>
+	は鼻母音にはならず、普通に <span class="font-ipa">/Vm/, /Vn/, /Vŋ/</span>（<span class="font-ipa"
+		>/V/</span
+	>
+	は任意の母音）などと読む。
+</p>
+<p>
+	例. <i>lüvjan</i> <span class="font-ipa">/ˈlyːʋjõː/</span>（「愛する」原形不定詞） vs.
+	<i>tu lüvjenn</i> <span class="font-ipa">/tʊ ˈlyːʋjɤn/</span>（tu不定詞）
+</p>
+<p>
+	また、母音字にオゴネク（“˛”
+	U+0328）が付いた場合、同様に鼻母音で読む。母音の変わり方は上と同様である。
+</p>
+<p>
+	なお、<strong>重音字の母音は後ろに <i>m, n</i> が来ても、鼻母音にはならない。</strong
+	>重音字の鼻母音は1番目の母音にオゴネクをつけて表す（「重音字の発音」の項で示したように。）
+</p>
+
+<h2 class="border-b-3 border-double ps-1">その他事項</h2>
+<p>ドイツ語などのように語末子音が無声化することはなく、文字通りに発音する。</p>
+<p>
+	語形変化などによって有声子音の後に無声子音が来るときは、無声化する。<span class="font-ipa"
+		>/ʋ/</span
+	>
+	は <span class="font-ipa">/f/</span> となる。
+</p>
+<p>
+	例. <i>flewgan</i> <span class="font-ipa">/ˈflɪɔɡõː/</span>（「飛ぶ」原形不定詞）→ <i>fljagst</i>
+	<span class="font-ipa">/flɪɐkst/</span>（直説法過去2人称単数）
+</p>
+<p>
+	例2. <i>draivan</i> <span class="font-ipa">/ˈdɽaɪʋõː/</span>（「運転する」原形不定詞）→
+	<i>draivst</i> <span class="font-ipa">/dɽaɪfst/</span>（直説法現在2人称単数）
+</p>
+
+<h5 class="text-center text-[red] text-3xl my-8">工事中...</h5>
--- src/routes/(layout1)/vaes/phonology/+page.svelte
+++ src/routes/(layout1)/vaes/phonology/+page.svelte
@@ -1,10 +1,397 @@
 <script lang="ts">
-
 </script>
 
 <svelte:head>
-    <title>葦語 音韻論</title>
+	<title>葦語 音韻論</title>
 </svelte:head>
 
-<h2 class="my-8 text-center">音韻論</h2>
-<p class="text-red-600 text-xl text-center">工事中……</p>
+<h2 class="text-center my-8">音韻論</h2>
+<p>以下の説明では、音声記号にはIPA（国際音声記号）を基にした音声記号を使用する。</p>
+<p>
+	<span class="font-ipa">“[]”</span> で囲ったものがIPAの精密な音声表記で、<span class="font-ipa"
+		>“//”</span
+	> で囲ったものはヴェッセンズラン語の音韻論に準じた音素表記である。
+</p>
+<section aria-labelledby="consonant">
+	<h2 id="consonant" class="border-b-3 border-double ps-1">子音</h2>
+
+	<div class="table-container">
+		<table class="grid-cols-[repeat(9,auto)] [&_td]:font-ipa [&_td]:text-center">
+			<thead>
+				<tr>
+					<th scope="col" class="col-span-2">＼</th>
+					<th scope="col">両唇</th>
+					<th scope="col">唇歯</th>
+					<th scope="col">歯/歯茎</th>
+					<th scope="col">そり舌</th>
+					<th scope="col">後部歯茎/硬口蓋</th>
+					<th scope="col">軟口蓋</th>
+					<th scope="col">声門</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th scope="row">破裂音</th>
+					<th scope="row">無声</th>
+					<td>p</td>
+					<td></td>
+					<td>t</td>
+					<td>(ʈ)</td>
+					<td></td>
+					<td>k</td>
+					<td></td>
+				</tr>
+				<tr>
+					<th scope="row" class="col-2">有声</th>
+					<td>b</td>
+					<td></td>
+					<td>d</td>
+					<td>(ɖ)</td>
+					<td></td>
+					<td>ɡ</td>
+					<td>(ʔ)</td>
+				</tr>
+				<tr>
+					<th scope="row" class="col-2">前声門化</th>
+					<td>ʔ͡p</td>
+					<td></td>
+					<td>ʔ͡t</td>
+					<td>(ʔ͡ʈ)</td>
+					<td></td>
+					<td>ʔ͡k</td>
+					<td></td>
+				</tr>
+				<tr>
+					<th scope="row" class="col-span-2">破擦音</th>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td>(t͡ɕ)</td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<th scope="row">摩擦音</th>
+					<th scope="row">無声中央</th>
+					<td></td>
+					<td>f</td>
+					<td>s</td>
+					<td>(ʂ)</td>
+					<td>ɕ</td>
+					<td>x</td>
+					<td></td>
+				</tr>
+				<tr>
+					<th class="col-2">無声側面</th>
+					<td></td>
+					<td></td>
+					<td>ɬ</td>
+					<td>(ɭ̝̊)</td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<th scope="row" class="col-2">有声</th>
+					<td></td>
+					<td></td>
+					<td>z</td>
+					<td>(ʐ)</td>
+					<td>ʑ</td>
+					<td></td>
+					<td>ɦ</td>
+				</tr>
+				<tr>
+					<th scope="row" class="col-span-2">鼻音</th>
+					<td>m</td>
+					<td></td>
+					<td>n</td>
+					<td>(ɳ)</td>
+					<td>ɲ</td>
+					<td>ŋ</td>
+					<td></td>
+				</tr>
+				<tr>
+					<th scope="row" class="col-span-2">はじき音</th>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td>ɽ</td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<th scope="row">接近音</th>
+					<th scope="row">中央</th>
+					<td></td>
+					<td>ʋ</td>
+					<td>ð</td>
+					<td>(ɻ)</td>
+					<td>j</td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<th scope="row" class="col-2">側面</th>
+					<td></td>
+					<td></td>
+					<td>l</td>
+					<td>(ɭ)</td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+
+	<h3>説明</h3>
+	<p>破裂音は有声・無声・前声門化の3系列の対立がある。前声門化破裂音は語頭には現れない。</p>
+	<p>
+		無声破裂音 <span class="font-ipa">/p/, /t/, /k/</span> は <span class="font-ipa">/s/</span>
+		の後でない音節頭にある時、帯気する (<span class="font-ipa">[pʰ], [tʰ], [kʰ]</span
+		>)。流音・接近音
+		<span class="font-ipa">/l/, /ɽ/, /j/, /ʋ/</span>
+		が後続する時、後続する音は無声音化する (<span class="font-ipa">[pɽ̊]</span> etc.)。
+	</p>
+	<p>
+		前声門化破裂音は帯気しない。前声門化破裂音の実際の発音は方言や年代によって様々であり、入破音 (<span
+			class="font-ipa">[ɓ], [ɗ], [ɠ]</span
+		>) や放出音 (<span class="font-ipa">[pʼ], [tʼ], [kʼ]</span>)
+		、朝鮮語の濃音に近いような音にもなり得る。
+	</p>
+	<p>
+		<span class="font-ipa">/ð/</span> は歯間〜歯接近音 <span class="font-ipa">[ð̪͆˕ ~ ð̞]</span> である。
+	</p>
+	<div>
+		<p>
+			<span class="font-ipa">/ɽ/</span> は前後の環境により実際の発音が変わり、また前後の子音の発音を変えることもある。
+		</p>
+		<ul>
+			<li>
+				<span class="font-ipa">/d/, /t/, /ʔ͡t/, /n/, /l/, /ɬ/</span> の前ではそり舌接近音
+				<span class="font-ipa">[ɻ]</span>
+				となり、各歯茎音も調音位置が同化して
+				<span class="font-ipa">[ɻɖ], [ɻʈ], [ɻʔ͡ʈ], [ɻɳ], [ɻɭ], [ɻɭ̝̊]</span> のようになる。
+			</li>
+			<li>
+				後に <span class="font-ipa">/s/, /z/</span> があるとき、同化してそれぞれ
+				<span class="font-ipa">[ɽʂ], [ɽʐ]</span> となる。
+			</li>
+			<li>
+				<span class="font-ipa">/ɬɽ/</span> は <span class="font-ipa">[ɭ̝̊ɽ]</span> のようになる。
+			</li>
+			<li>
+				<span class="font-ipa">/ɪ/, /iː/</span> に挟まれた環境では <span class="font-ipa">/ɽ/</span>
+				は歯茎はじき音 <span class="font-ipa">[ɾ]</span> のようになる。
+			</li>
+		</ul>
+	</div>
+	<p>
+		<span class="font-ipa">/x/</span> は実際は前部軟口蓋〜口蓋垂の間で変動する。<span
+			class="font-ipa">[x̟ ~ χ]</span
+		>
+	</p>
+</section>
+
+<section aria-labelledby="vowel">
+	<h2 id="vowel" class="border-b-3 border-double ps-1">母音</h2>
+	<h3 class="ms-1">単母音</h3>
+
+	<div class="table-container">
+		<table class="grid-cols-[repeat(8,auto)]">
+			<caption>口母音</caption>
+			<thead>
+				<tr>
+					<th scope="col">＼</th>
+					<th scope="col" class="col-span-4">前舌</th>
+					<th scope="col">中舌</th>
+					<th scope="col" class="col-span-2">後舌</th>
+				</tr>
+				<tr>
+					<th scope="col" class="col-start-2 col-end-4">非円唇</th>
+					<th scope="col" class="col-start-4 col-end-6">円唇</th>
+				</tr>
+				<tr>
+					<th scope="col" class="col-start-2">tense</th>
+					<th scope="col">lax</th>
+					<th scope="col">tense</th>
+					<th scope="col">lax</th>
+					<th scope="col">lax</th>
+					<th scope="col">tense</th>
+					<th scope="col">lax</th>
+				</tr>
+			</thead>
+			<tbody class="[&_td]:font-ipa [&_td]:text-center">
+				<tr>
+					<th scope="row">狭</th>
+					<td>iː</td>
+					<td>ɪ</td>
+					<td>yː</td>
+					<td>ʏ</td>
+					<td></td>
+					<td>uː</td>
+					<td>ʊ</td>
+				</tr>
+				<tr>
+					<th scope="row">半狭</th>
+					<td>eː</td>
+					<td>e</td>
+					<td>øː</td>
+					<td></td>
+					<td></td>
+					<td>oː</td>
+					<td></td>
+				</tr>
+				<tr>
+					<th scope="row">曖昧</th>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td>(ɤ)</td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<th scope="row">半広</th>
+					<td>ɛː</td>
+					<td>ɛ</td>
+					<td></td>
+					<td>œ</td>
+					<td>ɐ</td>
+					<td></td>
+					<td>ɔ</td>
+				</tr>
+				<tr>
+					<th scope="row">広</th>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td>ɑː</td>
+					<td></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+
+	<div class="table-container">
+		<table class="grid-cols-[repeat(7,auto)]">
+			<caption class="text-center fw-bolder">鼻母音</caption>
+			<thead>
+				<tr>
+					<th scope="col">＼</th>
+					<th scope="col" class="col-span-4">前舌</th>
+					<th scope="col" class="col-span-2">後舌</th>
+				</tr>
+				<tr>
+					<th scope="col" class="col-start-2 col-end-4">非円唇</th>
+					<th scope="col" class="col-start-4 col-end-6">円唇</th>
+				</tr>
+				<tr>
+					<th scope="col" class="col-start-2">tense</th>
+					<th scope="col">lax</th>
+					<th scope="col">tense</th>
+					<th scope="col">lax</th>
+					<th scope="col">tense</th>
+					<th scope="col">lax</th>
+				</tr>
+			</thead>
+			<tbody class="[&_td]:font-ipa [&_td]:text-center">
+				<tr>
+					<th scope="row">狭</th>
+					<td>ẽː</td>
+					<td>ẽ</td>
+					<td>ø̃ː</td>
+					<td></td>
+					<td>õː</td>
+					<td></td>
+				</tr>
+				<tr>
+					<th scope="row">曖昧</th>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td>(ɤ̃)</td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<th scope="row">広</th>
+					<td>ɛ̃ː</td>
+					<td>ɛ̃</td>
+					<td></td>
+					<td>œ̃</td>
+					<td></td>
+					<td>ɔ̃</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+
+	<h3>二重母音</h3>
+
+	<div class="table-container">
+		<table class="grid-cols-[repeat(5,auto)]">
+			<thead>
+				<tr>
+					<th scope="col" class="col-span-2">＼</th>
+					<th scope="col" class="col-span-3">終点</th>
+				</tr>
+				<tr class="*:font-ipa">
+					<th scope="col" class="col-start-3">ɪ</th>
+					<th scope="col">ʏ</th>
+					<th scope="col">ʊ</th>
+				</tr>
+			</thead>
+			<tbody class="[&_td]:font-ipa [&_td]:text-center">
+				<tr>
+					<th scope="row">始点</th>
+					<th scope="row">半広</th>
+					<td>ɛɪ̯</td>
+					<td>œʏ̯</td>
+					<td></td>
+				</tr>
+				<tr>
+					<th scope="row" class="col-start-2">広</th>
+					<td>aɪ̯</td>
+					<td></td>
+					<td>ɑʊ̯</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+
+	<h3>説明</h3>
+	<p>
+		ヴェッセンズラン語の母音には <strong class="italic">tense</strong> と
+		<strong class="italic">lax</strong>
+		と呼ばれる区別があり、<i>lax</i> 母音は別の母音や単語末に来ることはできない。ただし、曖昧母音の
+		<span class="font-ipa">/ɤ/</span> に限っては語末に来ることができる。
+	</p>
+	<p>二重母音は全て <i>tense</i> 母音である。</p>
+	<p>
+		<span class="font-ipa">/ɛː/</span> は実際は <span class="font-ipa">[ɛæ̯]</span> のような発音である。
+	</p>
+	<p>
+		<span class="font-ipa">/eː/</span> は実際は <span class="font-ipa">[eː] ~ [ei̯]</span> のような発音である。
+	</p>
+	<p>
+		<span class="font-ipa">/oː/</span> は実際は <span class="font-ipa">[oː] ~ [ou̯]</span> のような発音である。
+	</p>
+	<p>
+		<span class="font-ipa">/øː/</span> は実際は <span class="font-ipa">[øː] ~ [øy̯]</span> のような発音である。
+	</p>
+	<p>
+		<span class="font-ipa">/ɤ/</span> はアクセントの無い音節で
+		<span class="font-ipa">/e(ː)/, /ɛ(ː)/</span>
+		の異音 (allophone) として現れ、実際の音声は <span class="font-ipa">[ɘ] ~ [ɤ̟]</span> である。
+	</p>
+	<p>対応する鼻母音も同様。</p>
+</section>
+
+<h4 class="text-center my-8 text-3xl text-[red]">工事中...</h4>
