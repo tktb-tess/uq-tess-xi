f056197b76d7f206ef2f9b5e6b95b71cd711a629
Born-Bohrer <chem1000leaves@gmail.com>
2025-05-14 16:00:07 +0900

    aaaaa

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
 	import '../app.css';
-	const { children } = $props();
+	const { children } = $props(), linkCardUrl = '/image/link-card.png';
 </script>
 
 <svelte:head>
 	<!-- OGP -->
 	<meta property="og:url" content="https://tktb-tess.dev" />
 	<meta property="og:site_name" content="悠久肆方体" />
-	<meta property="og:image" content="/image/link-card.png" />
+	<meta property="og:image" content="{linkCardUrl}" />
 	<!-- Twitter card -->
 	<meta name="twitter:card" content="summary" />
 	<meta name="twitter:site" content="@triethylamineq" />
-	<meta name="twitter:image" content="/image/link-card.png" />
+	<meta name="twitter:image" content="{linkCardUrl}" />
 </svelte:head>
 
 {@render children()}
