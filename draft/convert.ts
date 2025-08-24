2aa200b6159435d55662e7cf29b470af8fdb368e
Born-Bohrer <chem1000leaves@gmail.com>
2025-08-24 13:44:16 +0900

    hi

--- draft/convert.ts
+++ draft/convert.ts
@@ -4,8 +4,6 @@ import { readFile, writeFile } from 'node:fs/promises';
 const names = ['miller-rabin'] as const;
 
 const main = async () => {
-	
-
 	const tasks = names.map(async (name) => {
 		const file = await readFile(`draft/${name}.md`, { encoding: 'utf-8' });
 		const htmlParsed = marked.parse(file, { breaks: true, async: false, gfm: true });
