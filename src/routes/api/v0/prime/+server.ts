a80ab18a70f9603f03360b0d6b715af3b921d3c2
Born-Bohrer <chem1000leaves@gmail.com>
2025-08-24 13:42:32 +0900

    iroiro

--- src/routes/api/v0/prime/+server.ts
+++ src/routes/api/v0/prime/+server.ts
@@ -1,45 +1,52 @@
 import { getRandPrimeByRange } from '@tktb-tess/util-fns';
-import { error, json } from '@sveltejs/kit';
+import { error, isHttpError, json } from '@sveltejs/kit';
+import { z } from 'zod';
 
 export type Primes = {
 	p: string;
 	q: string;
 };
+
 const LIMIT = 1n << 64n;
+
+const paramSchema = z.coerce.bigint().gte(0n).lte(LIMIT);
+
 export const GET = async ({ url }) => {
 	const params = url.searchParams;
 
 	const min = (() => {
 		const pre = params.get('min');
-		if (!pre) error(400);
-		const min = BigInt(pre);
-		if (min < 0n || min > LIMIT) error(400);
-		return min;
+		return paramSchema.parse(pre);
 	})();
 
 	const max = (() => {
 		const pre = params.get('max');
-		if (!pre) error(400);
-		const max = BigInt(pre);
-		if (max < 0n || max > LIMIT) error(400);
-		return max;
+		return paramSchema.parse(pre);
 	})();
 
 	try {
 		const p = getRandPrimeByRange(min, max);
 		const q = getRandPrimeByRange(min, max);
-		const headers = {
-			'Content-Type': 'application/json'
-		} as const;
 
 		const body: Primes = {
 			p: p.toString(),
 			q: q.toString()
 		};
 
-		return json(body, { headers });
+		return json(body);
 	} catch (e) {
-		if (e instanceof Error) {
+		if (isHttpError(e)) {
+			error(e.status, e.body);
+		} else if (e instanceof z.ZodError) {
+			const { name, issues } = e;
+			const { errors } = z.treeifyError(e);
+			const err = {
+				name,
+				issues,
+				errors
+			} as const;
+			return json(err, { status: 400 });
+		} else if (e instanceof Error) {
 			error(500, { message: `${e.name}: ${e.message}` });
 		} else {
 			error(500);
