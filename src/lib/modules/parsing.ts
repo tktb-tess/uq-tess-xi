0cd0c7609e51dfbd50dde9ce78f66d9520df112b
Born-Bohrer <chem1000leaves@gmail.com>
2025-09-18 10:01:12 +0900

    一旦

---
+++ src/lib/modules/parsing.ts
@@ -0,0 +1,43 @@
+import z from 'zod';
+import { ok, err, Result } from 'neverthrow';
+
+const parse_err_brand = Symbol('parse-error');
+
+type ParseError = {
+  readonly name: 'ParseError';
+  readonly message: string;
+  readonly cause: unknown;
+  readonly [parse_err_brand]: typeof parse_err_brand;
+};
+
+const ParseError = {
+  from(message: string, cause: unknown): ParseError {
+    return {
+      name: 'ParseError',
+      message,
+      cause,
+    } as ParseError;
+  },
+};
+
+export { ParseError };
+
+export const safeResultParse = <TSchema extends z.ZodType>(
+  schema: TSchema,
+  json: string,
+): Result<z.infer<TSchema>, ParseError | z.ZodError<z.infer<TSchema>>> => {
+  const safeJSONParse = Result.fromThrowable(JSON.parse, (e) => {
+    return ParseError.from('failed to parse', e);
+  });
+
+  return safeJSONParse(json).andThen(
+    (obj): Result<z.infer<TSchema>, z.ZodError<z.infer<TSchema>>> => {
+      const r = schema.safeParse(obj);
+
+      if (!r.success) {
+        return err(r.error);
+      }
+      return ok(r.data);
+    },
+  );
+};
