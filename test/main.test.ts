1df888a0f7b9103e1d8124e572a618b967d63187
Born-Bohrer <chem1000leaves@gmail.com>
2025-10-07 20:43:48 +0900

    docc

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,9 +1,7 @@
 import { mdToHtml } from '../src/lib/modules/md-html';
-import { it, expect } from 'vitest';
+import { it } from 'vitest';
 
 it('test1', async () => {
   const h = await mdToHtml(`# Hello, Markdown!`);
   console.log(h);
-
-  expect(0).toBe(0);
 });
