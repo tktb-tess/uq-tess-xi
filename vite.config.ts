518659cdd21775695f4dc4cf9658e4f2a3ea9365
tktb-tess <chemusiclove6174@gmail.com>
2026-01-08 12:39:42 +0900

    修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 /// <reference types="vitest/config" />
 import tailwindcss from '@tailwindcss/vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+  server: {
+    port: 8000,
+  },
+  preview: {
+    port: 8000,
+  },
   optimizeDeps: {
     exclude: [],
   },
