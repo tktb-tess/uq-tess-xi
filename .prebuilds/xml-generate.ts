e06f2a9ce80c10d9adc70cf82d4856812c73b32c
tktb-tess <chemusiclove6174@gmail.com>
2026-01-18 14:46:15 +0900

    xml

--- .prebuilds/xml-generate.ts
+++ .prebuilds/xml-generate.ts
@@ -1,47 +1,28 @@
-import { writeFileSync } from 'node:fs';
+import { writeFileSync, globSync } from 'node:fs';
 
-const paths: readonly string[] = [
-  '/',
-  '/conlang/',
-  '/conlang/vaes/',
-  '/conlang/vaes/letter-et-pron/',
-  '/conlang/vaes/noun/',
-  '/conlang/vaes/numeral/',
-  '/conlang/vaes/phonology/',
-  '/conlang/vaes/swadesh-list/',
-  '/data/',
-  '/data/conlang-xcumon/',
-  '/data/OE-verb-conj-type/',
-  '/others/',
-  '/others/gregorian-chants/',
-  '/others/ja-tibetan/',
-  '/others/license/',
-  '/others/md-converter/',
-  '/others/musik/',
-  '/others/tools/',
-  '/others/miller-rabin/',
-  '/others/like-programmer/',
-];
+const paths = (() => {
+  const gl_ = globSync(`./src/routes/**/+page.svelte`);
+  const paths = gl_.map((p) => {
+    return p.match(/src\/routes\/(?:\(layout[12]\))?((?:\/[^/]+)+\/)\+page\.svelte/u)?.[1] ?? '/';
+  });
+  return paths.sort();
+})();
 
-const main = () => {
-  const lastmod = new Date().toISOString();
+const lastmod = new Date().toISOString();
 
-  const urls = paths.map(
-    (p) => `  <url>
-      <loc>${new URL(p, 'https://www.tktb-tess.dev').href}</loc>
-    	<lastmod>${lastmod}</lastmod>
+const urls = paths.map(
+  (p) => `  <url>
+    <loc>${new URL(p, 'https://www.tktb-tess.dev').href}</loc>
+    <lastmod>${lastmod}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>${p === '/' ? 1 : 0.8}</priority>
   </url>`,
-  );
+);
 
-  const text = `<?xml version="1.0" encoding="utf-8" ?>
+const text = `<?xml version="1.0" encoding="utf-8" ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls.join('\n')}
 </urlset>`;
 
-  writeFileSync('./static/sitemap.xml', text);
-  console.log('sitemap.xml was successfully generated');
-};
-
-main();
+writeFileSync('./static/sitemap.xml', text);
+console.log('sitemap.xml was successfully generated');
