2db5a82c8a7c6d221fd6536dd3e3d3c6e530b2e1
tktb-tess <chemusiclove6174@gmail.com>
2026-02-16 16:00:04 +0900

    se

--- src/routes/(layout1)/+layout.svelte
+++ src/routes/(layout1)/+layout.svelte
@@ -58,8 +58,8 @@
   @reference '../../app.css';
   @layer components {
     .__container {
-      --max-w-main: 96rem;
-      --min-w-side: 17rem;
+      --max-w-main: 80rem;
+      --min-w-side: 18rem;
 
       grid-template-areas:
         'header header header'
