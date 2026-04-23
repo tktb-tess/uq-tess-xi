fc6b540b347dea35b259f22c149c1ee48be9669c
tktb-tess <chemusiclove6174@gmail.com>
2026-04-23 15:38:10 +0900

    iroiro

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -2,6 +2,6 @@ import type { HandleClientError, ClientInit } from '@sveltejs/kit';
 
 export const init: ClientInit = async () => {};
 
-export const handleError: HandleClientError = async ({ error, status, event, message }) => {
-  console.error(status, message, error, event);
+export const handleError: HandleClientError = async ({ error }) => {
+  console.error(error);
 };
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,6 +2,6 @@ import type { HandleServerError, ServerInit } from '@sveltejs/kit';
 
 export const init: ServerInit = async () => {};
 
-export const handleError: HandleServerError = async ({ error, status, event, message }) => {
-  console.error(status, message, error, event);
+export const handleError: HandleServerError = async ({ error }) => {
+  console.error(error);
 };
